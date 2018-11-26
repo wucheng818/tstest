27c93493eeb840fad35b8406a983e38a9ab6676d
orangeWU818wucheng <orangewu818@163.com>
2018-11-26 14:30:40 +0800

    书上说

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,61 +1,61 @@
-var arr: number[] = [1,2,3,4,5,6,7,8]
-// every方法
-let everys=(t:number)=>{
-    var b=true
-    for(let n of arr){
-        if(n<t){
-            b = false
-        }
-        return b
-    }
-}
+// var arr: number[] = [1,2,3,4,5,6,7,8]
+// // every方法
+// let everys=(t:number)=>{
+//     var b=true
+//     for(let n of arr){
+//         if(n<t){
+//             b = false
+//         }
+//         return b
+//     }
+// }
 
-console.log(everys(0))
+// console.log(everys(0))
 
-// some方法
-let somes=(t:number)=>{
-    var b=false
-    for(let n of arr){
-        if(n>t){
-            b = true
-        }
-    }
-    return b
-}
-console.log(somes(10))
+// // some方法
+// let somes=(t:number)=>{
+//     var b=false
+//     for(let n of arr){
+//         if(n>t){
+//             b = true
+//         }
+//     }
+//     return b
+// }
+// console.log(somes(10))
 
-//entries方法
+// //entries方法
 
-var arrEN: string[] = ['a','b','c']
+// var arrEN: string[] = ['a','b','c']
 
-let entriess=(arry:string[])=>{
-    var obj:object = {}
-    for(let index in arry){
-        let arr:string[]=[];
-        arr.push(index)
-        arr.push(arry[index])
-        obj[index]=arr
-    }
-    return obj
-}
-console.log(entriess(arrEN))
+// let entriess=(arry:string[])=>{
+//     var obj:object = {}
+//     for(let index in arry){
+//         let arr:string[]=[];
+//         arr.push(index)
+//         arr.push(arry[index])
+//         obj[index]=arr
+//     }
+//     return obj
+// }
+// console.log(entriess(arrEN))
 
-// find方法
+// // find方法
 
-let finds=(n:number)=>{
-    var x:string | number='undefined'
-    for(let t of arr){
-        if(t>n){
-            return x=t      
-        }
-    }
-    return x
-}
+// let finds=(n:number)=>{
+//     var x:string | number='undefined'
+//     for(let t of arr){
+//         if(t>n){
+//             return x=t
+//         }
+//     }
+//     return x
+// }
 
-console.log(finds(5))
+// console.log(finds(5))
 
-// map方法
+// // map方法
 
-class ExtendFin extends Array {
-    css
-}
+// class ExtendFin extends Array {
+//     css
+// }
