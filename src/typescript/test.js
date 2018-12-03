69940bf159b2b41a1f8d7cab9bc9c4a872fd667a
orangeWU818wucheng <orangewu818@163.com>
2018-12-03 18:01:15 +0800

    提交123

--- src/typescript/test.js
+++ src/typescript/test.js
@@ -124,7 +124,9 @@ new Observer(data);
 // 监听data对象的name属性，当data.name发现变化的时候，触发cb函数
 
 new Watcher(data, 'name', (oldValue, newValue) => {
+  
   console.log(oldValue, newValue);
+
 })
 
 data.name = 'zht';
